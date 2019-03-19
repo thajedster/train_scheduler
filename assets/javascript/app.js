f242deae69e994f244264a083853f897f389b167
thajedster <jedlawrencecheng@gmail.com>
2019-03-19 05:21:44 -0400

    created front end and initialized firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBjapi8UEVeT_1F7fnOcKdPxKs1-U-b9eg",
+    authDomain: "trainscheduler-246f2.firebaseapp.com",
+    databaseURL: "https://trainscheduler-246f2.firebaseio.com",
+    projectId: "trainscheduler-246f2",
+    storageBucket: "trainscheduler-246f2.appspot.com",
+    messagingSenderId: "165406524808"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+})
