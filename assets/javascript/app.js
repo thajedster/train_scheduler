939c094e4f95234ab36898222ab54665ecada0c3
thajedster <jedlawrencecheng@gmail.com>
2019-03-19 05:28:51 -0400

    added button on click funtion

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,5 +11,32 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+#("#submit").on("click", function (event) {
+    event.preventDefault();
 
-})
+    var trnName = $("#train_name").val().trim();
+    var destination = $("#dtn").val().trim();
+    var first_tt = $("#first_tt").val().trim();
+    var freq = $("#freq").val().trim();
+
+    var train = {
+        trn: trnName,
+        dest: destination,
+        start: first_tt,
+        frequency: freq
+    };
+
+    database.ref().push(train);
+
+    console.log(train.trnName);
+    console.log(train.dest);
+    console.log(train.start);
+    console.log(train.frequency);
+
+    alert("train added");
+
+    $("#train_name").val("");
+    $("#dtn").val("");
+    $("#first_tt").val("");
+    $("#freq").val("");
+});
